7a18513f8ce12390b6f5b9e843fb2461be2be0db
Sebi95 <burchidragomirsebastian14@stud.ase.ro>
2018-08-29 00:57:06 +0300

    First commit IxRide server

---
+++ src/main/java/keysight/ixia/hackathon/ixride_server/IxrideServerApplication.java
@@ -0,0 +1,12 @@
+package keysight.ixia.hackathon.ixride_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IxrideServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IxrideServerApplication.class, args);
+	}
+}
